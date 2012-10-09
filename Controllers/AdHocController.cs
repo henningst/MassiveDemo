36f752c0d988a8453ccd6682691c9c92148843bb
Henning Støverud <henning@stoverud.no>
2012-10-09 22:15:23 +0200

    Some cleanup

--- Controllers/AdHocController.cs
+++ Controllers/AdHocController.cs
@@ -1,4 +1,4 @@
-﻿using MassiveDemo.Domain;
+﻿using MassiveDemo.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Models/Product.cs
+++ Models/Product.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace MassiveDemo.Domain
+namespace MassiveDemo.Models
 {
     public class Product
     {
